45db85ff3fb6bd4905ab22cd9db7b9a825291ca8
Sistemas <157807149+JPCOMPAN@users.noreply.github.com>
2025-03-12 11:09:34 -0300

    repondendo_questoes_lista4

---
+++ Exercicios_Lista4/src/ex43.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ex43 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a letra da sua musica favorita: ");
+        String entrada = scanner.nextLine();
+        List<String> palavras = new ArrayList<>();
+
+        for(String palavra: entrada.split(" ") ){
+            palavras.add(palavra);
+        }
+        Collections.reverse(palavras);
+        String palavrasInvestidas = String.join(" ", palavras);
+        System.out.println(palavrasInvestidas);
+
+    }
+}
